820162d254ef1432ea1c69fbccfa9f877692a4d9
积木 <thewindyweather@gmail.com>
2017-12-30 22:25:57 +0800

    Update index.js
    add `?_inline` picture require

--- index.js
+++ index.js
@@ -14,36 +14,41 @@ module.exports = function(fileContent) {
 	else fileContent = JSON.stringify(fileContent);
 
 	if(query.deep !== false) fileContent = loadDeep(fileContent, this.query);
-	
-	return "module.exports = "+replaceSrc(fileContent, query.exclude);
+
+    fileContent = replaceSrc(fileContent, query.exclude);
+    fileContent = replaceInline(fileContent, query.exclude);
+	return "module.exports = "+fileContent;
 };
 
 
 function replaceSrc(fileContent, exclude) {
-    fileContent = fileContent.replace(/((\<img[^\<\>]*? src)|(\<link[^\<\>]*? href))[\s]*=[\s]*\\?[\"\']?[^\'\"\<\>\+]+?\\?[\'\"][^\<\>]*?[/]?\>/ig, function(str){
-    
-    var reg = /\s+((src)|(href))[\s]*=[\s]*\\?[\'\"][^\"\']+\\?[\'\"]/i;
-        var regResult = reg.exec(str);
+	fileContent = fileContent.replace(/((\<img[^\<\>]*? src)|(\<link[^\<\>]*? href))=\\?[\"\']?[^\'\"\<\>\+]+?\\?[\'\"][^\<\>]*?\>/ig, function(str){
+		var reg = /((src)|(href))=\\?[\'\"][^\"\']+\\?[\'\"]/i;
+		var regResult = reg.exec(str);
+		if(!regResult) return str;
+		var attrName = /\w+=/.exec(regResult[0])[0].replace('=', '');
+		var imgUrl = regResult[0].replace(attrName+'=', '').replace(/[\\\'\"]/g, '');
+		if(!imgUrl) return str; // 避免空src引起编译失败
+		if(/^(http(s?):)?\/\//.test(imgUrl)) return str; // 绝对路径的图片不处理
+		if(!/\.(jpg|jpeg|png|gif|svg|webp)/i.test(imgUrl)) return str; // 非静态图片不处理
+		if(exclude && imgUrl.indexOf(exclude) != -1) return str; // 不处理被排除的
+		if(!(/^[\.\/]/).test(imgUrl)) {
+			imgUrl = './' + imgUrl;
+		}
+		return str.replace(reg, attrName+"=\"+JSON.stringify(require("+JSON.stringify(imgUrl)+"))+\"");
+	});
+	return fileContent;
+}
 
-        if(!regResult) return str;
-        var attrName = /\w+\s*=\s*/.exec(regResult[0])[0].replace(/\s*=\s*$/, '');
-        var imgUrl = regResult[0].replace(/\w+\s*=\s*/, '').replace(/[\\\'\"]/g, '');
-        if(!imgUrl) return str; // 避免空src引起编译失败
+function replaceInline(fileContent, exclude) {
+    fileContent = fileContent.replace(/\\?["'](\S*?\.(jpg|jpeg|png|gif|svg|webp))\?_inline.*?["']/ig, function(match, str){
+    	var imgUrl = str;
         if(/^(http(s?):)?\/\//.test(imgUrl)) return str; // 绝对路径的图片不处理
-        if(!/\.(jpg|jpeg|png|gif|svg|webp)/i.test(imgUrl)) return str; // 非静态图片不处理
         if(exclude && imgUrl.indexOf(exclude) != -1) return str; // 不处理被排除的
-    	imgUrl = imgUrl.replace(/^\s*/g, ''); // 去掉左边空格
-
-        if(!(/^[\.\/]/).test(imgUrl)) {
-            imgUrl = './' + imgUrl;
-        }
-	var res = str.replace(reg, " "+attrName+"=\"+JSON.stringify(require("+JSON.stringify(imgUrl)+"))+\"");
-
-        return res;
+        return "\"+JSON.stringify(require("+JSON.stringify(imgUrl)+"))+\"";
     });
     return fileContent;
 }
-
 
 function loadDeep(fileContent, queryStr) {
 	return fileContent.replace(/#include\(\\?[\'\"][^\'\"]+\\?[\'\"]\);?/g, function(str){
